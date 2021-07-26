0f77ea072679e5407cadd26a1b4dd8acf83fa0c6
mattyydagz <mnjdagostino@gamil.com>
2021-07-26 15:59:02 -0400

    Added smooth scrolling

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 "use strict";
 
+// Smooth Scrolling
+const navLinkh = document.querySelector("#nav-h");
+const navLink1 = document.querySelector("#nav-1");
+const navLink2 = document.querySelector("#nav-2");
+const sectionh = document.querySelector("#section-h");
 const section1 = document.querySelector("#section-1");
-const nav = document.querySelector(".navbar");
-const navLinks = document.querySelector(".nav-links");
+const section2 = document.querySelector("#section-2");
+
+navLinkh.addEventListener("click", function (e) {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+navLink1.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset - 25,
+    behavior: "smooth",
+  });
+});
+
+navLink2.addEventListener("click", function (e) {
+  const s2coords = section2.getBoundingClientRect();
+
+  window.scrollTo({
+    left: s2coords.left + window.pageXOffset,
+    top: s2coords.top + window.pageYOffset - 25,
+    behavior: "smooth",
+  });
+});
 
 // Show Navbar
+const nav = document.querySelector(".navbar");
+const navLinks = document.querySelector(".nav-links");
 const header = document.querySelector(".header");
 const navHeight = nav.getBoundingClientRect().height;
 
